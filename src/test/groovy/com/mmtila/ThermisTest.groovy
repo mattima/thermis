ae2f535ab54807c32f607f9b54738e5318a742e3
Markus Mattila <mmtila@gmail.com>
2022-11-25 14:57:57 +0000

    Initial commit

---
+++ src/test/groovy/com/mmtila/ThermisTest.groovy
@@ -0,0 +1,19 @@
+package com.mmtila
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class ThermisTest {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Test
+    void testItWorks() {
+        assert application.running == true
+    }
+
+}
